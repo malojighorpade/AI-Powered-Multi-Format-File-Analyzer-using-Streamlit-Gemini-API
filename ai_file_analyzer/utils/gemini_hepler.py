e195523dc7aeff2b55e76e7255673f83aab06c93
Maloji <malojighorpade07@gmail.com>
2026-02-13 18:03:20 +0530

    secrets are changed

--- ai_file_analyzer/utils/gemini_hepler.py
+++ ai_file_analyzer/utils/gemini_hepler.py
@@ -1,22 +1,26 @@
-
-import google.generativeai as gen
+from google import genai as gen
 import os
 from dotenv import load_dotenv
+import streamlit as st
+
 load_dotenv()
-gen.configure(api_key=os.getenv("Gem-api-key"))
-text_model=gen.GenerativeModel("gemini-2.5-flash")
-image_model=gen.GenerativeModel("gemini-2.5-flash")
+
+# Create client instead of configure (new SDK way)
+client = gen.Client(api_key=st.secrets["GEMINI_API_KEY"])
+
+text_model = "gemini-2.5-flash"
+image_model = "gemini-2.5-flash"
 
 
-Max_prompt=4000
+Max_prompt = 4000
 
-def build_prompt(extracted_content,user_prompt):
+def build_prompt(extracted_content, user_prompt):
     """
     Build structured prompt for gemini
     """
-    clean_content=extracted_content[:Max_prompt]
+    clean_content = extracted_content[:Max_prompt]
 
-    formetted_promt=f"""
+    formetted_promt = f"""
 You are an AI file analysis assisitent .format User INstruction:
 {user_prompt}
 File_content:
@@ -29,25 +33,31 @@ Instruction:
 """
     return formetted_promt.strip()
 
+
 def generate_text_response(promt):
     """
     send structured promt to gemini
     and return clean response text
     """
     try:
-        response=text_model.generate_content(promt)
+        response = client.models.generate_content(
+            model=text_model,
+            contents=promt
+        )
         return response.text
     except Exception as e:
         return f"Errror genrating response:{str(e)}"
 
 
-def generate_image_response(image,user_prompt):
+def generate_image_response(image, user_prompt):
     """
     send image+instruction to gemini
-
     """
     try:
-        response=image_model.generate_content([user_prompt,image])
+        response = client.models.generate_content(
+            model=image_model,
+            contents=[user_prompt, image]
+        )
         return response.text
     except Exception as e:
         return f"Error genrating image response:{str(e)}"
